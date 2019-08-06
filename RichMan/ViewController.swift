d1ba5dc9b3e267c83f851f0b9b89bb8eb6cf77d2
Yuan Chang <yuanchang_chang@intuit.com>
2019-08-06 10:41:32 -0700

    Initial Commit

---
+++ RichMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RichMan
+//
+//  Created by Chang, Yuan Chang on 8/6/19.
+//  Copyright © 2019 fds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
